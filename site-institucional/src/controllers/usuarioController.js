6e171a7ea22f902ffb1b5feb6f688b4c955f9ef5
VS-Sousa <vinicius.sousa@sptech.school>
2022-04-25 10:03:32 -0300

    Criando serviço de edição de usuários

--- site-institucional/src/controllers/usuarioController.js
+++ site-institucional/src/controllers/usuarioController.js
@@ -29,6 +29,56 @@ function entrar(request, response) {
   }
 }
 
+function editar(request, response) {
+  var id = request.params.idUsuario;
+
+  var nome = request.body.nomeServer;
+  var cnpj = request.body.cnpjServer;
+  var email = request.body.emailServer;
+  var telefone = request.body.telefoneServer;
+  var senha = request.body.senhaServer;
+
+  if (id < 1000 || id == undefined) {
+    response.status(400).send("Id inválido!");
+  } else if (nome == undefined) {
+    response.status(400).send("Seu nome está undefined!");
+  } else if (cnpj == undefined) {
+    response.status(400).send("Seu CNPJ está undefined!");
+  } else if (email == undefined) {
+    response.status(400).send("Seu email está undefined!");
+  } else if (telefone == undefined) {
+    response.status(400).send("Seu telefone está undefined!");
+  } else if (senha == undefined) {
+    response.status(400).send("Sua senha está undefined!");
+  } else {
+    usuarioModel.editar(id, nome, email, senha, cnpj, telefone).then(function (resposta) {
+      if (resposta.affectedRows < 1) {
+        response.status(404).send('Houve um erro ao atualizar os dados')
+      } else if (resposta.affectedRows > 1) {
+        response.status(500).send('Houve um erro ao atualizar os dados')
+      } else {
+        usuarioModel.buscarPorId(id).then(function (usuario) {
+          console.log(usuario);
+          response.json(usuario[0]);
+        }).catch(function (erro) {
+          console.log(erro);
+          console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+          response.status(500).json(erro.sqlMessage);
+        })
+      }
+
+    }).catch(function (erro) {
+      console.log(erro);
+      console.log(
+          "\nHouve um erro ao realizar o atualizar! Erro: ",
+          erro.sqlMessage
+      );
+      
+      response.status(500).json(erro.sqlMessage);
+    })
+  }
+}
+
 module.exports = {
   entrar
 }
--- site-institucional/src/models/usuarioModel.js
+++ site-institucional/src/models/usuarioModel.js
@@ -1,7 +1,7 @@
 var database = require('../database/config');
 
 function entrar(email, senha) {
-  console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", email, senha);
+  console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar():", email, senha);
 
   var instrucao = `
     SELECT * FROM usuario WHERE emailUsuario = '${email}' AND senhaUsuario = SHA2('${senha}', 512);
@@ -11,6 +11,37 @@ function entrar(email, senha) {
   return database.executar(instrucao);
 }
 
+function editar(id, nome, email, senha, cnpj, telefone) {
+  console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function editar():", id, nome, email, senha, cnpj, telefone);
+
+  var instrucao = `
+    UPDATE usuario 
+    SET nomeUsuario = '${nome}', 
+    emailUsuario = '${email}', 
+    senhaUsuario = SHA2('${senha}', 512), 
+    cnpjUsuario = '${cnpj}', 
+    telefoneUsuario = '${telefone}' 
+    WHERE idUsuario = ${id}
+  `;
+
+  console.log("Executando a instrução SQL: \n" + instrucao);
+  return database.executar(instrucao);
+}
+
+function buscarPorId(id) {
+  console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function buscarPorId():", id);
+
+  var instrucao = `
+    SELECT * FROM usuario WHERE idUsuario = ${id}
+  `;
+
+  console.log("Executando a instrução SQL: \n" + instrucao);
+  return database.executar(instrucao);
+  
+}
+
 module.exports = {
-  entrar
+  entrar,
+  editar,
+  buscarPorId
 };
