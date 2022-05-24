c419401ce4554a50c4188bedad363d38057eaede
JoaoVictorHengler <joao.hengler@sptech.school>
2022-05-23 22:23:39 -0300

    Arrumando Registro

--- site-institucional/src/routes/registro.js
+++ site-institucional/src/routes/registro.js
@@ -4,10 +4,10 @@ var router = express.Router();
 var registroController = require('../controllers/registroController');
 
 router.get('/obterDados/:fkEntrega', function (request, response) {
-  registroController.listarPorFarmaceutica(request, response);
+  registroController.obter(request, response);
 })
 
-router.get('/obterDados/limite/:fkEntrega&:limite', function (request, response) {
+router.get('/obterDados/limite/:fkEntrega&:ordenar&:limite', function (request, response) {
   registroController.cadastrarMedicamento(request, response);
 });
 
